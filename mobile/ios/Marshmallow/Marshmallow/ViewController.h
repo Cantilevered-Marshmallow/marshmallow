36f65ad8188b313f4e15867b6b7025aa4ebeca33
Brandon Borders <brandon@camelcased.com>
2015-10-15 11:28:09 -0700

    Setup iOS codebase

---
+++ mobile/ios/Marshmallow/Marshmallow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Marshmallow
+//
+//  Created by Brandon Borders on 10/15/15.
+//  Copyright © 2015 Cantilevered Marshmallow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mobile/ios/Marshmallow/MarshmallowTests/MarshmallowTests.m
@@ -0,0 +1,39 @@
+//
+//  MarshmallowTests.m
+//  MarshmallowTests
+//
+//  Created by Brandon Borders on 10/15/15.
+//  Copyright © 2015 Cantilevered Marshmallow. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MarshmallowTests : XCTestCase
+
+@end
+
+@implementation MarshmallowTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
