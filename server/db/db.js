4fd97a74c526661fb8180447d09d96bf1aa14a46
brianleung <bleungwh@gmail.com>
2015-10-16 15:47:20 -0700

    Add env vars to db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 var Sequelize = require('Sequelize');
 
-module.exports = new Sequelize('marshmallow', 'username', 'password', {
-  host: 'localhost',
+module.exports = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_SERVER,
   dialet: 'mysql'
 });
