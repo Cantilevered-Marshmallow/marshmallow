b72df10694efd81230542a81243148db439794a0
brianleung <bleungwh@gmail.com>
2015-10-19 17:23:57 -0700

    Fix server specs.

---
+++ server/user/userModel.js
@@ -0,0 +1,11 @@
+var Sequelize = require('Sequelize');
+var sequelize = require(__dirname + '/../db/db');
+
+var User = sequelize.define('user', {
+  email: {type: Sequelize.STRING, unique: true},
+  facebookId: {type: Sequelize.STRING, unique: true}
+});
+
+User.sync({force:true});
+
+module.exports = User;
